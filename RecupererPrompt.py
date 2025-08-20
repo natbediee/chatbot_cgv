652af560ff3abf2d2e6649bb510ace9b15f49149
natbediee <natbediee@gmail.com>
2025-08-20 10:45:50 +0200

    tes modifs locales

--- RecupererPrompt.py
+++ RecupererPrompt.py
@@ -1,5 +1,14 @@
 import PresenceBdd
-# recuperation du prompt de la question utilisateur
-PromptUser= input("Quelle est votre question ?")
-# on va vérifier dans notre BDD que cette question n'a pas déjà été posée
-PresenceBdd.PresenceBdd(PromptUser)
+
+def main():
+
+    # recuperation du prompt de la question utilisateur
+    PromptUser= input("Quelle est votre question ?")
+    if not PromptUser:
+            print("Aucune question saisie.")
+            return
+    # on va vérifier dans notre BDD que cette question n'a pas déjà été posée
+    PresenceBdd.PresenceBdd(PromptUser)
+
+if __name__ == "__main__":
+    main()
